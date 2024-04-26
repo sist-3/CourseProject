4a6c7733532a0c8d55adde53c0de4f1063da60b1
KYBYK <dbqks56231106@gmail.com>
2024-04-26 12:48:07 +0900

    Fix : 페이지 PageManager 등록, 성혁 xml 추가, subject.xml 삭제

--- src/util/PageManager.java
+++ src/util/PageManager.java
@@ -1,18 +1,27 @@
 package util;
 
-
 import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 import client.Main;
+import page.ExamAllListManagementPage;
 import page.ExamManagementPage;
+import page.ExamScoreListManagementPage;
+import page.ExamSelectListManagementPage;
 import page.LoginPage;
+import page.ProfessorManagementPage;
+import page.StudentExamListManagementPage;
+import page.StudentManagementPage;
+import page.StudentMyPage;
+import page.SubjectManagementPage;
+import page.panel.QuizMultiplePanel;
+import page.panel.QuizSubjectivePanel;
 
 public class PageManager {
 
-private Main main;
-	
+	private Main main;
+
 	private static ArrayList<JPanel> pageList = new ArrayList<>();
 	private static ArrayList<String> pageNameList = new ArrayList<>();
 
@@ -30,28 +39,42 @@ private Main main;
 	public void init(Main main) {
 		setMain(main);
 		setPage();
+		setComponentPage();
 	}
-		
+
 	private void setMain(Main main) {
 		this.main = main;
 	}
-	
+
 	private void setPage() {
+		addPage(new ExamAllListManagementPage());
 		addPage(new ExamManagementPage());
+		addPage(new ExamScoreListManagementPage());
+		addPage(new ExamSelectListManagementPage());
 		addPage(new LoginPage());
+		addPage(new ProfessorManagementPage());
+		addPage(new StudentExamListManagementPage());
+		addPage(new StudentManagementPage());
+		addPage(new StudentMyPage());
+		addPage(new SubjectManagementPage());
 	}
-	
+
+	private void setComponentPage() {
+		addPage(new QuizMultiplePanel());
+		addPage(new QuizSubjectivePanel());
+	}
+
 	private void addPage(JPanel page) {
 		pageList.add(page);
 		pageNameList.add(page.getClass().getSimpleName());
 	}
-	
+
 	public ArrayList<JPanel> getPage() {
 		return pageList;
 	}
 
 	public void changePage(String name) {
-		if(pageNameList.contains(name)) {			
+		if (pageNameList.contains(name)) {
 			main.pageCard.show(main.page, name);
 		} else {
 			System.out.println("등록되지 않은 페이지거나 오타가 있습니다.");
