fea40e9cbd5546cec85a8543f8957f6aa123761b
geummo <kummo99@naver.com>
2024-04-26 17:49:05 +0900

    과목 정보 추가

--- src/dialog/AddSubjectDialog.java
+++ src/dialog/AddSubjectDialog.java
@@ -7,39 +7,81 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import page.SubjectManagementPage;
+import vo.StudentVO;
+import vo.SubjectVO;
+
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.ImageIcon;
 import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class AddSubjectDialog extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField name_tf;
+	private JTextField point_tf;
+	private JTextField file_tf;
+	SubjectManagementPage p;
+	SubjectVO vo;
+	JButton okButton;
+	JButton cancelButton;
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		try {
-			AddSubjectDialog dialog = new AddSubjectDialog();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	/**
 	 * Create the dialog.
+	 * 
+	 * @wbp.parser.constructor
 	 */
-	public AddSubjectDialog() {
+	public AddSubjectDialog(SubjectManagementPage p) {
+		this.p = p;
+		init();
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String sb_name = name_tf.getText().trim();
+				String sb_point = point_tf.getText().trim();
+				String sb_plan_file = file_tf.getText().trim();
+
+				SubjectVO vo = new SubjectVO();
+
+				vo.setSb_name(sb_name);
+				vo.setSb_point(sb_point);
+				vo.setSb_plan_file(sb_plan_file);
+
+				int cnt = p.addSubject(vo);
+
+				if (cnt > 0) {
+					JOptionPane.showMessageDialog(AddSubjectDialog.this, "저장완료!");
+					dispose();
+				}
+			}
+		});
+		cancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+	}
+
+	public AddSubjectDialog(SubjectVO vo) {
+		this.vo = vo;
+		init();
+		viewDialog();
+	}
+
+	private void init() {
 		setBounds(100, 100, 361, 300);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -58,10 +100,10 @@ public class AddSubjectDialog extends JDialog {
 				panel.add(lblNewLabel);
 			}
 			{
-				textField = new JTextField();
-				textField.setBounds(106, 23, 206, 21);
-				panel.add(textField);
-				textField.setColumns(10);
+				name_tf = new JTextField();
+				name_tf.setBounds(106, 23, 206, 21);
+				panel.add(name_tf);
+				name_tf.setColumns(10);
 			}
 			{
 				JLabel lblNewLabel = new JLabel("과목학점:");
@@ -70,10 +112,10 @@ public class AddSubjectDialog extends JDialog {
 				panel.add(lblNewLabel);
 			}
 			{
-				textField_1 = new JTextField();
-				textField_1.setColumns(10);
-				textField_1.setBounds(106, 61, 206, 21);
-				panel.add(textField_1);
+				point_tf = new JTextField();
+				point_tf.setColumns(10);
+				point_tf.setBounds(106, 61, 206, 21);
+				panel.add(point_tf);
 			}
 			{
 				JLabel lblNewLabel = new JLabel("과목 시작일:");
@@ -160,15 +202,16 @@ public class AddSubjectDialog extends JDialog {
 				panel.add(lblNewLabel);
 			}
 			{
-				textField_2 = new JTextField();
-				textField_2.setColumns(10);
-				textField_2.setBounds(106, 181, 180, 21);
-				panel.add(textField_2);
+				file_tf = new JTextField();
+				file_tf.setColumns(10);
+				file_tf.setBounds(106, 181, 180, 21);
+				panel.add(file_tf);
 			}
 			{
 				JLabel lblNewLabel_1 = new JLabel("");
 				lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-				lblNewLabel_1.setIcon(new ImageIcon(AddSubjectDialog.class.getResource("/resources/image/filelink1.png")));
+				lblNewLabel_1
+						.setIcon(new ImageIcon(AddSubjectDialog.class.getResource("/resources/image/filelink1.png")));
 				lblNewLabel_1.setBounds(289, 182, 20, 20);
 				panel.add(lblNewLabel_1);
 			}
@@ -179,17 +222,26 @@ public class AddSubjectDialog extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("추가");
+				okButton = new JButton("저장");
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
-				JButton cancelButton = new JButton("취소");
+				cancelButton = new JButton("취소");
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+
+	private void viewDialog() {
+		name_tf.setText(vo.getSb_name());
+		point_tf.setText(vo.getSb_point());
+		file_tf.setText(vo.getSb_plan_file());
+
 	}
 
 }
