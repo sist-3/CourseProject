9a401026316a2fd32fcdda48bbb5e93f3e0f75ce
KYBYK <dbqks56231106@gmail.com>
2024-04-26 16:10:50 +0900

    Feat : SQL 로그인 기능 구현

--- src/dao/Yubin.java
+++ src/dao/Yubin.java
@@ -1,5 +1,29 @@
 package dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import util.MybatisManager;
+import vo.LoginVO;
+
 public class Yubin {
 
+	private SqlSessionFactory factory = MybatisManager.getInstance().getFactory();
+	private SqlSession ss;
+	
+	public LoginVO login(String id) {
+		ss = factory.openSession();
+		LoginVO login_mem = ss.selectOne("yubin.login", id);
+		close();
+		return login_mem;
+	}
+
+	private void close() {
+		if(ss != null) {
+			ss.close();
+		}
+	}
 }
