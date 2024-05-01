251e143fe2367907b7289134bbbadd400ed25163
KYBYK <dbqks56231106@gamil.com>
2024-05-01 16:56:28 +0900

    Feat : 원형차트에서 파이차트로 코드 수정

--- src/page/ProgressMonitoringManagementPage.java
+++ src/page/ProgressMonitoringManagementPage.java
@@ -5,19 +5,18 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 
-import component.bar.CircleBar;
+import component.piechart.ModelPieChart;
+import component.piechart.PieChart;
+
 import java.awt.Dimension;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.DropMode;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JComboBox;
 import java.awt.Font;
-import javax.swing.border.BevelBorder;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.EmptyBorder;
+
+import component.piechart.PieChart.PeiChartType;
 
 public class ProgressMonitoringManagementPage extends JPanel {
 
@@ -36,41 +35,50 @@ public class ProgressMonitoringManagementPage extends JPanel {
 		panel.setSize(new Dimension(800, 600));
 		add(panel, BorderLayout.CENTER);
 		panel.setLayout(null);
-		CircleBar circleBar = new CircleBar();
-		circleBar.setBorder(new EmptyBorder(5, 5, 5, 5));
-		circleBar.setBackground(Color.WHITE);
-		circleBar.setBounds(12, 189, 249, 237);
-		circleBar.setForeground(Color.RED);
-		panel.add(circleBar);
-		
-		CircleBar circleBar_1 = new CircleBar();
-		circleBar_1.setBorder(new EmptyBorder(5, 5, 5, 5));
-		circleBar_1.setBackground(Color.WHITE);
-		circleBar_1.setForeground(Color.RED);
-		circleBar_1.setBounds(278, 189, 249, 237);
-		panel.add(circleBar_1);
-		
-		CircleBar circleBar_2 = new CircleBar();
-		circleBar_2.setBorder(new EmptyBorder(5, 5, 5, 5));
-		circleBar_2.setBackground(Color.WHITE);
-		circleBar_2.setForeground(Color.RED);
-		circleBar_2.setBounds(545, 189, 249, 237);
-		panel.add(circleBar_2);
-		
+
 		JLabel lblNewLabel = new JLabel("시험 응시 현황");
 		lblNewLabel.setFont(new Font("KCC-한빛", Font.PLAIN, 15));
 		lblNewLabel.setBounds(12, 158, 140, 19);
 		panel.add(lblNewLabel);
+		
+		PieChart pieChart1 = new PieChart();
+		pieChart1.setLocation(0, 184);
+		pieChart1.setSize(new Dimension(262, 242));
+		pieChart1.setFont(new Font("프리젠테이션 4 Regular", Font.PLAIN, 19));
+		pieChart1.setChartType(PieChart.PeiChartType.DONUT_CHART);
+		pieChart1.addData(new ModelPieChart("제출", 0, makeColor()));
+		pieChart1.addData(new ModelPieChart("미제출", 0, makeColor()));
+		panel.add(pieChart1);
 		
 		JLabel lblNewLabel_1 = new JLabel("점수 분포");
 		lblNewLabel_1.setFont(new Font("KCC-한빛", Font.PLAIN, 15));
 		lblNewLabel_1.setBounds(278, 158, 140, 19);
 		panel.add(lblNewLabel_1);
 		
+		PieChart pieChart2 = new PieChart();
+		pieChart2.setSize(new Dimension(262, 242));
+		pieChart2.setFont(new Font("Dialog", Font.PLAIN, 19));
+		pieChart2.setChartType(PeiChartType.DONUT_CHART);
+		pieChart2.setBounds(263, 184, 262, 242);
+		pieChart2.addData(new ModelPieChart("0 ~ 39", 0, makeColor()));
+		pieChart2.addData(new ModelPieChart("40 ~ 59", 0, makeColor()));
+		pieChart2.addData(new ModelPieChart("60 ~ 79", 0, makeColor()));
+		pieChart2.addData(new ModelPieChart("80 ~ 100", 0, makeColor()));
+		panel.add(pieChart2);
+		
 		JLabel lblNewLabel_1_1 = new JLabel("합격 현황");
-		lblNewLabel_1_1.setFont(new Font("KCC-한빛", Font.PLAIN, 15));
+		lblNewLabel_1_1.setFont(new Font("KCC-한빛", Font.PLAIN, 19));
 		lblNewLabel_1_1.setBounds(545, 158, 140, 19);
 		panel.add(lblNewLabel_1_1);
+		
+		PieChart pieChart3 = new PieChart();
+		pieChart3.setSize(new Dimension(262, 242));
+		pieChart3.setFont(new Font("Dialog", Font.PLAIN, 19));
+		pieChart3.setChartType(PeiChartType.DONUT_CHART);
+		pieChart3.setBounds(538, 184, 262, 242);
+		pieChart3.addData(new ModelPieChart("합격", 15, new Color(23, 126, 238)));
+		pieChart3.addData(new ModelPieChart("불합격", 5, new Color(221, 65, 65)));
+		panel.add(pieChart3);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBackground(Color.WHITE);
@@ -117,5 +125,13 @@ public class ProgressMonitoringManagementPage extends JPanel {
 		table.getColumnModel().getColumn(1).setPreferredWidth(210);
 		table.getColumnModel().getColumn(2).setPreferredWidth(66);
 		table.getColumnModel().getColumn(3).setPreferredWidth(62);
+	}
+	
+	private Color makeColor() {
+		
+		int r = (int)(Math.random() * 255);
+		int g = (int)(Math.random() * 255);
+		int b = (int)(Math.random() * 255);
+		return new Color(r, g, b);
 	}
 }
