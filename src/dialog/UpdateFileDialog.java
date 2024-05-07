054fa6034dbfb144d0da06237b3e4a3c36efde13
geummo <kummo99@naver.com>
2024-05-07 10:32:12 +0900

    강의계획서 파일 변경 dialog창 추가 저장경로에있는 저장된 파일 내용은 변경한 파일내용으로 변경

---
+++ src/dialog/UpdateFileDialog.java
@@ -0,0 +1,62 @@
+package dialog;
+
+import java.awt.TextField;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class UpdateFileDialog extends JDialog {
+
+    private static final long serialVersionUID = 1L;
+    private String selectedFilePath;
+    private String selectedFileName;
+    private UpdateSubjectDialog parent;
+    private String FileName;
+    //파일업로드창
+    public UpdateFileDialog(UpdateSubjectDialog parent, String subjectName) {
+    	 
+    	
+    	this.parent = parent;
+        JFileChooser fileChooser = new JFileChooser();
+        int returnValue = fileChooser.showOpenDialog(parent);
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("txt 파일", "txt");
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            FileName = selectedFile.getName(); //원래파일명
+            selectedFileName = subjectName + ".txt"; // 과목명을 파일명에 추가
+            selectedFilePath = "src/resources/subplan/" + selectedFileName;
+           
+            File newFile = new File(selectedFilePath);
+            
+            
+            // 선택한 파일을 복사
+             try {
+                 Files.copy(selectedFile.toPath(), newFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+             } catch (IOException e) {
+                e.printStackTrace();
+             }
+        } else {
+            // 사용자가 파일을 선택하지 않았을 경우, 선택한 파일 경로와 파일 이름을 null로 설정
+            selectedFilePath = null;
+            selectedFileName = null;
+        }
+    }
+
+    public String getSelectedFilePath() {
+        return selectedFilePath;
+    }
+
+    public String getSelectedFileName() {
+        return selectedFileName;
+    }
+    public String getFileName() {
+        return FileName;
+    }
+
+}
